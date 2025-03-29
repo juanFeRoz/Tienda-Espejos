8b1582975fc879dd24d089a17c78051914865c2d
Jadavid22 <davidvillanueva979@gmail.com>
2025-03-29 16:15:51 -0500

    Initial commit

---
+++ Backend/proyectoespejos/src/main/java/com/uis/proyectoespejos/ProyectoespejosApplication.java
@@ -0,0 +1,13 @@
+package com.uis.proyectoespejos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoespejosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoespejosApplication.class, args);
+	}
+
+}
---
+++ Backend/proyectoespejos/src/test/java/com/uis/proyectoespejos/ProyectoespejosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uis.proyectoespejos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoespejosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
